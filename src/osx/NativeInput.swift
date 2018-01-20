f13214bfdf1d4af57cde5ec75e431f35ae8f0f81
mnemoto <mnemoto.dev@gmail.com>
2018-01-20 22:32:42 +0900

    changed to call the callback after the process

--- src/osx/NativeInput.swift
+++ src/osx/NativeInput.swift
@@ -1,26 +1,35 @@
-@objc(NativeInput) class NativeInput : CDVPlugin {
+@objc(NativeInput) class NativeInput: CDVPlugin {
     func echo(_ command: CDVInvokedUrlCommand) {
-        var pluginResult = CDVPluginResult(
-                status: CDVCommandStatus_ERROR
-        )
 
-        let msg = command.arguments[0] as? String ?? ""
+        self.commandDelegate.run(inBackground: {
+            let msg = command.arguments[0] as? String ?? ""
 
-        if msg.characters.count > 0 {
-            // Swiftの場合
-            let alert = NSAlert()
-            alert.messageText = "入力エラー"
-            alert.informativeText = msg
-            alert.addButton(withTitle: "わかりました")
-            alert.runModal()
+            if msg.characters.count > 0 {
+                // Swiftの場合
+//                let alert = NSAlert()
+//                alert.messageText = "入力エラー"
+//                alert.informativeText = msg
+//                alert.addButton(withTitle: "わかりました")
+//                alert.runModal()
 
 
+                let pluginResult: CDVPluginResult = CDVPluginResult(
+                        status: CDVCommandStatus_OK,
+                        messageAs: msg
+                )
+                pluginResult.setKeepCallbackAs(true)
 
-            pluginResult = CDVPluginResult(
-                    status: CDVCommandStatus_OK,
-                    messageAs: msg
-            )
-        }
+                self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+                print("done")
+                pluginResult.setKeepCallbackAs(false)
+                self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+
+            }
+        })
+
+        let pluginResult: CDVPluginResult = CDVPluginResult(status: CDVCommandStatus_NO_RESULT)
+        pluginResult.setKeepCallbackAs(true)
+
 
         self.commandDelegate!.send(
                 pluginResult,
